2f79a210b011204f66e01d8c59f78fc2b341a94d
Himanshu Srivastava <himanshus4965@gmail.com>
2025-10-30 23:50:44 +0000

    Automated backup on 20251030_235000

--- hrms/ars/doctype/overtime/overtime.py
+++ hrms/ars/doctype/overtime/overtime.py
@@ -5,14 +5,47 @@
 from frappe.model.document import Document
 import frappe
 from frappe import _
+import datetime
+
+from datetime import timedelta
 
 class Overtime(Document):
 	def validate(self):
 		self.validate_hours()
+		self.validate_holiday()
 	
 	def validate_hours(self):
 		for row in self.overtime_table:
 			row.validate_time()
+
+
+	def validate_holiday(self):
+		if self.shift == "所定休日":
+			if isinstance(self.start_date, str):
+				start_time = datetime.datetime.strptime(self.start_date, "%Y-%m-%d %H:%M:%S")
+			else:
+				start_time = self.start_date
+			if isinstance(self.end_date, str):
+				end_dt = datetime.datetime.strptime(self.end_date, "%Y-%m-%d %H:%M:%S")
+			else:
+				end_dt = self.end_date
+			start_limit = start_time.replace(hour=5, minute=0, second=0, microsecond=0)
+			end_limit = start_limit + timedelta(days=1)  # next day 5:00 AM
+
+			# Check if start_time is within allowed window
+			if not (start_limit < end_dt <= end_limit):
+				frappe.throw(_("これが午前5時に達したら、別の申請を出してください。."))
+			if self.overtime_table:
+				#start_date = self.overtime_table[0].start_time
+				# if isinstance(start_date,str):
+				# 	start_time = datetime.datetime.strptime(start_date, "%Y-%m-%d %H:%M:%S")
+				# else:
+				# 	start_time = start_date
+				#start_limit = start_time.replace(hour=5, minute=0, second=0, microsecond=0)
+				#end_limit = start_limit + timedelta(days=1)  # next day 5:00 AM
+				for row in self.overtime_table:
+					row.validate_holiday(start_limit,end_limit)
+
 
 
 @frappe.whitelist()
--- hrms/ars/doctype/overtime_work_details_table/overtime_work_details_table.py
+++ hrms/ars/doctype/overtime_work_details_table/overtime_work_details_table.py
@@ -2,9 +2,25 @@
 # For license information, please see license.txt
 import frappe
 from frappe.model.document import Document
+import datetime
 
 class Overtimeworkdetailstable(Document):
 	def validate_time(self):
 		#frappe.msgprint(f"Validating time: {self.end_time} - {self.start_time}")
 		if str(self.end_time) <= str(self.start_time):
 			frappe.throw(frappe._("End date/time must be later than Start date/time."))
+	def validate_holiday(self,start_limit,end_limit):
+		#frappe.msgprint(f"Validating time: {self.end_time} - {self.start_time}")
+		start_dt = (
+		datetime.datetime.strptime(self.start_time, "%Y-%m-%d %H:%M:%S")
+		if isinstance(self.start_time, str)
+		else self.start_time
+		)
+		end_dt = (
+		datetime.datetime.strptime(self.end_time, "%Y-%m-%d %H:%M:%S")
+		if isinstance(self.end_time, str)
+		else self.end_time
+		)
+		if not (start_limit < end_dt <= end_limit):
+			frappe.throw(frappe._(f" これが午前5時に達したら、別の申請を出してください。"))
+
